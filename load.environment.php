2b9dd90a4fde5264ae8fb5cc41360b32d9a1d9fc
iamanh1990 <quocanh1990bach@gmail.com>
2021-12-03 13:02:59 +0200

    Dotenv not found error

--- load.environment.php
+++ load.environment.php
@@ -2,10 +2,12 @@
 
 /**
  * This file is included very early. See autoload.files in composer.json and
- * https://getcomposer.org/doc/04-schema.md#files
+ * https://getcomposer.org/doc/04-schema.md#files.
  */
 
 use Dotenv\Dotenv;
+
+require __DIR__.'/vendor/autoload.php';
 
 /**
  * Load any .env file. See /.env.example.
